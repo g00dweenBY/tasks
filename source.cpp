8f8a2f5686d959c8f8471d3f0221a555f7b9b311
Dmitry Simonov <gundalini335@gmail.com>
2023-03-26 00:09:12 +0300

    simple expression

--- source.cpp
+++ source.cpp
@@ -9,6 +9,16 @@ using std::endl;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	
+	int a, b, c;
+	cout << "Введите цифры a, b, c:\n";
+	cout << "a=";
+	cin >> a;
+	cout << "b=";
+	cin >> b;
+	cout << "c=";
+	cin >> c;
+	cout << "\nВычисляем по формуле: x=(a + b - c / a) + c * a * a - (a + b)";
+	double x = (a + b - c / a) + c * a * a - (a + b);
+	cout << "\nx=" << x;
 	return 0;
 }
