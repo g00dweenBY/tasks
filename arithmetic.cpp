119af8840f445fff1da6531821feb1915dbe8637
Dmitry Simonov <gundalini335@gmail.com>
2023-03-25 18:23:24 +0300

    moved the task

--- arithmetic.cpp
+++ arithmetic.cpp
@@ -10,4 +10,20 @@
 
 	double dVectorLength = sqrt(dX * dX + dY * dY + dZ * dZ);
 	cout << "\nДлина вектора = " << dVectorLength << endl;
+
+		int number;
+	cout << "\n\t\tДеление числа на разряды";
+	cout << "\nВведите пятизначное число: ";
+	cin >> number;
+
+	int n1 = number % 100000 / 10000;
+	int n2 = number % 10000 / 1000;
+	int n3 = number % 1000 / 100;
+	int n4 = number % 100 / 10;
+	int n5 = number % 10;
+	cout << "\n1 цифра равна " << n1;
+	cout << "\n2 цифра равна " << n2;
+	cout << "\n3 цифра равна " << n3;
+	cout << "\n4 цифра равна " << n4;
+	cout << "\n5 цифра равна " << n5;
 */
