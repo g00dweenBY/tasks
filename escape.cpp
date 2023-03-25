72d609ff25d5cd5b33fafc156882147b6ecfe14c
Dmitry Simonov <gundalini335@gmail.com>
2023-03-25 17:11:49 +0300

    escape task

---
+++ escape.cpp
@@ -0,0 +1,21 @@
+/*
+	int main()
+{
+	cout << "\t      " << "House" << endl;
+	cout << "\t        /\\" << endl;
+	cout << "\t       /  \\" << endl;
+	cout << "\t      /    \\" << endl;
+	cout << "\t     /      \\" << endl;
+	cout << "\t    /        \\" << endl;
+	cout << "\t   /----------\\" << endl;
+	cout << "\t     |      |" << endl;
+	cout << "\t     |      |" << endl;
+	cout << "\t     |      |" << endl;
+	cout << "\t     |      |" << endl;
+	cout << "\t     |______|" << endl;
+
+
+	cout << "*" << "\n**" << "\n***" << "\n****" << "\n*****" << "\n******" << endl;
+	return 0;
+}
+*/
