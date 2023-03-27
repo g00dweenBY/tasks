58f3f2438e0f4097dd6250abacb10de6b93842b5
Dmitry Simonov <gundalini335@gmail.com>
2023-03-27 18:14:42 +0300

    branching task 01

--- source.cpp
+++ source.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <math.h>
 #include <iomanip>
-
+#include <cmath>
 
 using namespace std;
-
+string global_str;
+int a;
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	
+
 	return 0;
 }
