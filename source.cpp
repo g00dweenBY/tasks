1fe58a51b0aa2843dd22eb2d040bf9c68790260c
Dmitry Simonov <gundalini335@gmail.com>
2023-04-11 11:57:42 +0300

    Корректировка задачи

--- source.cpp
+++ source.cpp
@@ -14,18 +14,7 @@ string global_str;
 int main()
 {	
 	setlocale(LC_ALL, "Russian");
-	string lineOne, lineTwo;
-	cout << "Введите первое слово: ";
-	cin >> lineOne;
-	cout << "Введите второе слово: ";
-	cin >> lineTwo;
-
-	if (lineOne.size() == lineTwo.size())
-		cout << "Ваши строки: " << lineOne << " и " << lineTwo << " равны" << endl;
-	if (lineOne.size() > lineTwo.size())
-		cout << "Строка : " << lineOne << " Больше, чем строка: " << lineTwo << endl;
-	if(lineOne.size() < lineTwo.size())
-		cout << "Строка : " << lineTwo << " Больше, чем строка: " << lineOne << endl;
+	
 
 
 
---
+++ string.cpp
@@ -0,0 +1,17 @@
+/*
+
+string lineOne, lineTwo;
+	cout << "Введите первое слово: ";
+	cin >> lineOne;
+	cout << "Введите второе слово: ";
+	cin >> lineTwo;
+
+	if (lineOne.size() == lineTwo.size())
+		cout << "Ваши строки: " << lineOne << " и " << lineTwo << " равны" << endl;
+	if (lineOne.size() > lineTwo.size())
+		cout << "Строка : " << lineOne << " Больше, чем строка: " << lineTwo << endl;
+	if(lineOne.size() < lineTwo.size())
+		cout << "Строка : " << lineTwo << " Больше, чем строка: " << lineOne << endl;
+
+
+*/
