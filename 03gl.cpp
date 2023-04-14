e355d83a031df2a9e3d18eda15c4b71e8243dc58
Dmitry Simonov <gundalini335@gmail.com>
2023-04-14 05:35:58 +0300

    Глава 3, задача 5. процентное соотношение численности земли

--- 03gl.cpp
+++ 03gl.cpp
@@ -10,4 +10,14 @@
 	const int secondsResults = seconds % 60;
 	cout << seconds << " секунд = " << days << " дней, " << hours << " часов, " << minutes << " минут, " << secondsResults << " секунд" << endl;
 
+	======================================================================
+	const long long worldPeoplePopulation = 7888000000;
+	const long  peopleBelarussian = 9255524;
+
+	cout << "Количество людей в мире: " << worldPeoplePopulation << '\n';
+	cout << "Количество людей в Беларуси: " << peopleBelarussian << '\n';
+
+	const double percentageOfWorld = static_cast<double>(peopleBelarussian) / worldPeoplePopulation * 100;
+
+	cout << "Процентное соотношеие населения Беларуси от всего мира составляет: " << percentageOfWorld << "%" << endl;
 */
--- source.cpp
+++ source.cpp
@@ -16,17 +16,8 @@ string global_str;
 int main()
 {	
 	setlocale(LC_ALL, "Russian");
-	long long seconds;
-	cout << "Введите количество секунд: ";
-	cin >> seconds;
 
-	const int days = seconds / 86400;
-	const int hours = seconds / 3600 % 24;
-	const int minutes = seconds / 60 % 60;
-	const int secondsResults = seconds % 60;
-	cout << seconds << " секунд = " << days << " дней, " << hours << " часов, " << minutes << " минут, " << secondsResults << " секунд" << endl;
-
-
+	
 
 	return 0;	
 
