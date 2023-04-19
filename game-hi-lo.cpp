26822397a744d5166953ecce118f219ceb1053af
Dmitry Simonov <gundalini335@gmail.com>
2023-04-19 15:15:00 +0300

    таска на ввод значения, и вывод значения из массива соотв введенного числа

--- game-hi-lo.cpp
+++ game-hi-lo.cpp
@@ -71,4 +71,62 @@ int gameHiLo()
 	}
 }
 
+
+ ------------------------------------------------------------------------------------------
+
+ int getRandomNumber(int min, int max)
+{
+	static const double fraction = 1.0 / (static_cast<double>(RAND_MAX) + 1.0);
+	// Равномерно распределяем выбор случайного числа в диапазоне
+	return static_cast<int>(rand() * fraction * (max - min + 1) + min);
+}
+
+bool playGame(int guesses, int number)
+{
+	for (int i = 1; i <= guesses; ++i)
+	{
+		cout << "Guess #" << i << ": ";
+		int guess;
+		cin >> guess;
+		if (guess > number)
+			cout << "Your guess is too high\n";
+		else if (guess < number)
+			cout << "Your guess is too low\n";
+		else
+			return true;
+	}
+	return false;
+}
+
+bool playAgain()
+{
+	char choisePlayer;
+
+	do
+	{
+		cout << "Would you like to play again (y/n)? ";
+		cin >> choisePlayer;
+	} while (choisePlayer != 'y' && choisePlayer  != 'n');
+
+	return (choisePlayer == 'y');
+}
+
+ srand(static_cast<unsigned int>(time(0)));
+	rand();
+
+	const int guesses = 7;
+	do
+	{
+		int randomNumber = getRandomNumber(1, 100);
+		cout << "Let's play a game. I'm thinking of a number. You have " << guesses << " tries to guess what is it\n";
+		bool won = playGame(guesses, randomNumber);
+		if (won)
+			cout << "Correct, you win!!\n";
+		else
+			cout << "Sorry, you lose. The correct number was " << randomNumber << "\n";
+
+	} while (playAgain());
+	cout << "Thank you for playing.\n";
+ ------------------------------------------------------------------------------------------
+
 */
--- source.cpp
+++ source.cpp
@@ -14,12 +14,30 @@ string global_str;
 
 
 
-
 int main()
 {	
-	srand(static_cast<unsigned int>(time(0)));
-
 	setlocale(LC_ALL, "Russian");
+	int number = 0;
+	int array[] = { 7,5,6,4,9,8,2,1,3 };
+	int arrLength = sizeof(array) / sizeof(array[0]);
+	cout << arrLength << endl;
+	do
+	{
+		cout << "¬введите пожалуйста число от 1 до 9: ";
+		cin >> number;
+
+		if (cin.fail())
+			cin.clear();
+	} while (number < 1 || number > 9);
+
+
+
+	for (int i = 0; i < arrLength; ++i) {
+		if (array[i] == number)
+			cout << "„исло " << number << " имеет индекс: " << i << endl;
+	}
+
+
 
 
 	return 0;	
