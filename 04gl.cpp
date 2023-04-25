0ee0aa8816228f4e90640aa7b3e7144d7a9237e3
Dmitry Simonov <gundalini335@gmail.com>
2023-04-25 10:44:40 +0300

    Задачи, решаемые в 4 главе

--- 04gl.cpp
+++ 04gl.cpp
@@ -45,4 +45,82 @@ Pizza *pizza = new Pizza[3];
 		"\nизготавливает пиццы диаметром = " << pizza[2].diametr <<
 		"\nи весом = " << pizza[2].weight << endl;
 
+
+
+		CandyBar snack = { "Ассорти", 2.4, 5550 };
+	CandyBar truffel = { "Трюфель", 3.8, 7843 };
+	CandyBar gercog = { "Герцог", 4.5, 9740 };
+	cout << "Название конфет: " << snack.name <<
+		"\nколичество грамм: " << snack.weight <<
+		"\nколичество калорий: " << snack.calories << endl;
+	CandyBar snacks[3] = { snack, truffel, gercog };
+	cout << "N1 = " << snacks[0].name << " " << snacks[0].weight << " " << snacks[0].calories << endl;
+	cout << "N2 = " << snacks[1].name << " " << snacks[1].weight << " " << snacks[1].calories << endl;
+	cout << "N3 = " << snacks[2].name << " " << snacks[2].weight << " " << snacks[2].calories << endl;
+
+	int arr[5] = { 1,3,5,7,9 };
+	int even = arr[0] + arr[4];
+	cout << arr[0] << " + " << arr[4] << " = " << even << endl;
+
+	string name = "Waldorf Salad";
+
+	Fish fish;
+	cout << "Тип рыбы: ";
+	cin >> fish.type;
+	cout << "Вес рыбы в килограммах: ";
+	cin >> fish.weight;
+	cout << "Длина рыбы в сантиметрах: ";
+	cin >> fish.length;
+
+	cout << "У вас имеется рыба: " << fish.type <<
+		"\nеё вес составляет: " << fish.weight <<
+		"\nдлина её составляет: " << fish.length << endl;
+
+		double ted = 2.2;
+	double* tedtpr = &ted;
+
+	cout << *tedtpr << endl;
+
+	float treacle[5] = { 1.1,2.2,3.3,4.4,5.5 };
+	float* firstTreacle = &treacle[0];
+	*firstTreacle = 1.5;
+	float* lastTreacle = &treacle[4];
+	cout << *firstTreacle << endl;
+	cout << *lastTreacle << endl;
+	cout << treacle[0] << endl;
+	int length;
+	cout << "Количество элементов в массиве, которое будет содержаться: ";
+	cin >> length;
+	int* arr = new int[length];
+	vector<int> arr1(length);
+
+
+	cout << "Как вас зовут? ";
+	string firstName;
+	cin >> firstName;
+	cout << "Какая у вас фамилия ? ";
+	string lastName;
+	cin >> lastName;
+	cout << "Что-то там? ";
+	string chto;
+	cin >> chto;
+	cout << "Сколько вам лет? ";
+	int age;
+	cin >> age;
+
+	cout << "Ваши инициалы: " << firstName << ", " << lastName <<
+		"\nчто-то там: " << chto <<
+		"\nвозраст: " << age << endl;
+
+
+	cout << "Введите ваше полное имя: ";
+	string fullName;
+	getline(cin, fullName);
+	cout << "Введите ваш любимый десерт: ";
+	string dessert;
+	getline(cin, dessert);
+
+	cout << "Вас зовут: " << fullName <<
+		"\nваш любиый десерт: " << dessert << endl;
+
 */
--- source.cpp
+++ source.cpp
@@ -23,10 +23,6 @@ struct  CandyBar
 	int calories;
 };
 
-
-
-
-
 int main()
 {	
 	setlocale(LC_ALL, "Russian");
@@ -45,81 +41,7 @@ int main()
 	array<char, 30> actor;
 	array<short, 100> betsie;
 	array<float, 13> chuck;
-	CandyBar snack = { "Ассорти", 2.4, 5550 };
-	CandyBar truffel = { "Трюфель", 3.8, 7843 };
-	CandyBar gercog = { "Герцог", 4.5, 9740 };
-	cout << "Название конфет: " << snack.name <<
-		"\nколичество грамм: " << snack.weight <<
-		"\nколичество калорий: " << snack.calories << endl;
-	CandyBar snacks[3] = { snack, truffel, gercog };
-	cout << "N1 = " << snacks[0].name << " " << snacks[0].weight << " " << snacks[0].calories << endl;
-	cout << "N2 = " << snacks[1].name << " " << snacks[1].weight << " " << snacks[1].calories << endl;
-	cout << "N3 = " << snacks[2].name << " " << snacks[2].weight << " " << snacks[2].calories << endl;
-
-	int arr[5] = { 1,3,5,7,9 };
-	int even = arr[0] + arr[4];
-	cout << arr[0] << " + " << arr[4] << " = " << even << endl;
-
-	string name = "Waldorf Salad";
-
-	Fish fish;
-	cout << "Тип рыбы: ";
-	cin >> fish.type;
-	cout << "Вес рыбы в килограммах: ";
-	cin >> fish.weight;
-	cout << "Длина рыбы в сантиметрах: ";
-	cin >> fish.length;
-
-	cout << "У вас имеется рыба: " << fish.type <<
-		"\nеё вес составляет: " << fish.weight <<
-		"\nдлина её составляет: " << fish.length << endl;
-
-		double ted = 2.2;
-	double* tedtpr = &ted;
-
-	cout << *tedtpr << endl;
-
-	float treacle[5] = { 1.1,2.2,3.3,4.4,5.5 };
-	float* firstTreacle = &treacle[0];
-	*firstTreacle = 1.5;
-	float* lastTreacle = &treacle[4];
-	cout << *firstTreacle << endl;
-	cout << *lastTreacle << endl;
-	cout << treacle[0] << endl;
-	int length;
-	cout << "Количество элементов в массиве, которое будет содержаться: ";
-	cin >> length;
-	int* arr = new int[length];
-	vector<int> arr1(length);
-
-
-	cout << "Как вас зовут? ";
-	string firstName;
-	cin >> firstName;
-	cout << "Какая у вас фамилия ? ";
-	string lastName;
-	cin >> lastName;
-	cout << "Что-то там? ";
-	string chto;
-	cin >> chto;
-	cout << "Сколько вам лет? ";
-	int age;
-	cin >> age;
-
-	cout << "Ваши инициалы: " << firstName << ", " << lastName <<
-		"\nчто-то там: " << chto <<
-		"\nвозраст: " << age << endl;
-
-
-	cout << "Введите ваше полное имя: ";
-	string fullName;
-	getline(cin, fullName);
-	cout << "Введите ваш любимый десерт: ";
-	string dessert;
-	getline(cin, dessert);
-
-	cout << "Вас зовут: " << fullName <<
-		"\nваш любиый десерт: " << dessert << endl;
+	
 
 	*/
 	
