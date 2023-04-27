cd39f4d931de5dda7a47b041aa485e3dc4126cac
Dmitry Simonov <gundalini335@gmail.com>
2023-04-27 22:45:19 +0300

    Задача 5.6

--- source.cpp
+++ source.cpp
@@ -29,5 +29,47 @@ int main()
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
 
+	
+	
+		array<string, 12> months = { "Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь", };
+	array<int, 12> booksQuantityFirstYear;
+
+	for (int i = 0; i < months.size(); i++)
+	{
+		cout << "Месяц: " << months[i] << "\n";
+		cout << "Введите количество проданных экземпляров книг за месяц " << months[i] << " ";
+		int sellingBooksQuantity;
+		cin >> sellingBooksQuantity;
+		cout << "Количество проданных книг за месяц " << months[i] << " составляет: " << sellingBooksQuantity << "\n\n" << endl;
+		booksQuantityFirstYear[i] = sellingBooksQuantity;
+	}
+
+	int sum = 0;
+
+	for (int i = 0; i < booksQuantityFirstYear.size(); i++)
+	{
+		cout << "Количество проданных экземляров за " << "\"" << months[i] << "\" : " << booksQuantityFirstYear[i] << endl;
+		sum += booksQuantityFirstYear[i];
+	}
+	cout << "\n\nГодовой объем продаж составил: " << sum << endl;	
+	
+
+
+	array<array<int, 12>, 3> sells = { {
+		booksQuantityFirstYear,
+		{11,55,24,32,56,76,89,54,23,24,77,11},
+		{12,24,23,26,1,3,3,5,7,8,9,1},
+	} };
+	int fullSum = 0;
+	cout << sells.size();
+
+	for (int i = 0; i < sells.size(); i++)
+	{
+		for (int j = 0; j < sells[j].size(); j++)
+		{
+			fullSum += sells[i][j];
+		}		
+	}
+	cout << "Итоговая сумма за 3 года составила: " << fullSum << endl;
 	return 0;
 }
