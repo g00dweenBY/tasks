a8e92b39c5d4d59bc905db9db21ba05a2142c60c
Dmitry Simonov <gundalini335@gmail.com>
2023-05-02 15:11:37 +0300

    Задача 6.2 заполнение массива числами, и последующие действия, описанные в задаче

--- source.cpp
+++ source.cpp
@@ -31,25 +31,42 @@ int main()
 	setlocale(LC_ALL, "Russian");
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
-	string words;
-	cout << "Введите пожалуйста строку: ";
-	getline(cin, words, '@');
-	for (int i = 0; i < words.length(); ++i)
-	{
-		if (!isdigit(words[i])) {
-			if (isupper(words[i])) {
-				cout << (char)tolower(words[i]);
-			}
-			else if (islower(words[i])) {
-				cout << (char)toupper(words[i]);
-			}
-			else {
-				cout << words[i];
-			}
-		}
-	}
-	
+	array<double, 5> arr;
+	double sum = 0;
+	int length = sizeof(arr) / sizeof(arr[0]);
+	int count = 0;
+	for (int i = 0; i < length; ++i)
+	{	
+		cout << "Введите значение с плавающей точкой: ";
+		
+		do
+		{
+			cin >> arr[i];
 
+			if (cin.fail())
+			{
+				cout << "Некорректный ввод. Попробутйе ещё раз." << endl;
+				cin.clear();
+				cin.ignore(10000, '\n');
+			}
+		} while (cin.fail());
+		cout << "Элеменет массива с индексом: " << i << " = " << arr[i] << endl;
+		sum += arr[i];
+	}
+	double average = sum / length;
+	cout << "Сумма равна: " << sum << endl;
+	cout << "Среднее значение элементов: " << average << endl;
+
+	cout << "Значения, которые больше среднего значения по массиву: ";
+	for (int i = 0; i < length; ++i)
+	{
+		if (arr[i] > average)
+		{
+			cout << arr[i] << " ";
+			++count;
+		}	
+	}
+	cout << "\nКоличество значения превышающих среднее: " << count << endl;
 	/*
 	char automobile[50];
 	int year;
