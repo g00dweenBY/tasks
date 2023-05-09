e97e10aa699dc19655a1406d0d56da537cf3eb52
Dmitry Simonov <gundalini335@gmail.com>
2023-05-10 02:00:52 +0300

    test structure

--- source.cpp
+++ source.cpp
@@ -1,108 +1,19 @@
-#include <iostream> // -- предназначена для ввода-вывода данных
-#include <fstream>
-#include <math.h> // -- математические ф-ии
-#include <iomanip> // -- предназачена для работы с форматированным выводом данных
-#include <cmath> // -- математическая С-шная
-#include <string> // -- предназначена для работы со строками
-#include <array> // -- предназначена для работы с массивами фиксированного размера. Предоставляет класс --array--
-#include <cstdlib> // -- для работы с памятью и процессом
-#include <algorithm> // -- содержит множество стандартных ф-ий для работы с алгоритмами
-#include <utility> // -- содержит различные утилиты, для решения повседневных задач
-#include <vector>
+#include <iostream>
 #include <Windows.h>
-#include "add.h"
-#include "constants.h"
-#include "boo.h"
-#include "doo.h"
 
-
-
-
-string global_str;
+#define PRINT_DMITRY
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
-	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	
+#ifndef PRINT_DMITRY
+	std::cout << "My name is Dmitry" << '\n';
+#endif // PRINT_DMITRY
 
 	
-	/*
-	char automobile[50];
-	int year;
-	double a_price;	
-	double b_price;
-	ofstream outFile;
-	outFile.open("carinfo.txt");
-	cout << "Enter the make and model of automobile: ";
-	cin.getline(automobile, 50);
-	cout << "Enter the model year: ";
-	cin >> year;
-	cout << "Enter the original asking price: ";
-	cin >> a_price;
-	b_price = 0.913 * a_price;
-	cout << fixed;
-	cout.precision(2);
-	cout.setf(ios_base::showpoint);
-	cout << "Make and model: " << automobile << endl;
-	cout << "Year: " << year << endl;
-	cout << "Was asking $" << a_price << endl;
-	cout << "Now asking $" << b_price << endl;
-
-	outFile << fixed;
-	outFile.precision(2);
-	outFile.setf(ios_base::showpoint);
-	outFile << "Make and model: " << automobile << endl;
-	outFile << "Year: " << year << endl;
-	outFile << "Was asking $" << a_price << endl;
-	outFile << "Now asking $" << b_price << endl;
-	outFile.close();
-
-
-	double value;
-	double sum = 0.0;
-	int count = 1;
-	inFile >> value;
-	while (inFile.good())
-	{
-		++count;
-		sum += value;
-		inFile >> value;
-	}
-	if (inFile.eof())
-	{
-		cout << "End of file reached.\n";
-	}
-	else if (inFile.fail())
-	{
-		cout << "Input terminated by data mismatch.\n";
-	}
-	else {
-		cout << "Input terminated for unknown reason.\n";
-	}
-	if (count == 0)
-	{
-		cout << "No data processed.\n";
-	}
-	else {
-		cout << "Items read: " << count << endl;
-		cout << "Sum: " << sum << endl;
-		cout << "Average: " << sum / count << endl;
-	}
-	inFile.close();
-
-	string fileName;
-	ifstream inFile;
-	cout << "Enter name of data file: ";
-	cin >> fileName;
-	inFile.open(fileName);
-	if (!inFile.is_open())
-	{
-		cout << "Could not open the file " << fileName << endl;
-		cout << "Programm terminating.\n";
-		exit(EXIT_FAILURE);
-	}
-	
-	*/
 	return 0;
 }
+
