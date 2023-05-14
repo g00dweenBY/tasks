e445cc9c4d4f62ab946befc9546cde3c2f37ddb7
Dmitry Simonov <gundalini335@gmail.com>
2023-05-15 02:01:48 +0300

    tasks 6b 6c

--- source.cpp
+++ source.cpp
@@ -7,10 +7,10 @@
 
 enum CardSuits
 {
-	SUIT_CLUBS,
-	SUIT_DIAMONDS,
-	SUIT_HEARTS,
-	SUIT_PEAKS,
+	SUIT_CHERVEY,
+	SUIT_BUBA,
+	SUIT_KRESTY,
+	SUIT_PIK,
 	MAX_SUITS,
 
 };
@@ -26,12 +26,52 @@ enum CardTypes
 	TYPE_EIGHT,
 	TYPE_NINE,
 	TYPE_TEN,
-	TYPE_JECK,
-	TYPE_LADY,
-	TYPE_KING,
-	TYPE_ACE,
-	MAX_TYPESM
+	TYPE_VALET,
+	TYPE_DAMA,
+	TYPE_KOROL,
+	TYPE_TUZ,
+	MAX_TYPES,
 };
+
+struct Card {
+	CardSuits suit;
+	CardTypes type;
+};
+
+void printCard(const Card& card)
+{
+
+	switch (card.type)
+	{
+	case TYPE_TWO: std::cout << "2"; break;
+	case TYPE_THREE: std::cout << "3"; break;
+	case TYPE_FOUR: std::cout << "4"; break;
+	case TYPE_FIVE: std::cout << "5"; break;
+	case TYPE_SIX: std::cout << "6"; break;
+	case TYPE_SEVEN: std::cout << "7"; break;
+	case TYPE_EIGHT: std::cout << "8"; break;
+	case TYPE_NINE: std::cout << "9"; break;
+	case TYPE_TEN: std::cout << "10"; break;
+	case TYPE_VALET: std::cout << "V"; break;
+	case TYPE_DAMA: std::cout << "D"; break;
+	case TYPE_KOROL: std::cout << "K"; break;
+	case TYPE_TUZ: std::cout << "T"; break;
+	default:
+		break;
+	}
+
+	switch (card.suit)
+	{
+	case SUIT_CHERVEY: std::cout << " червей"; break;
+	case SUIT_BUBA: std::cout << " бубей"; break;
+	case SUIT_KRESTY: std::cout << " крестей"; break;
+	case SUIT_PIK: std::cout << " пик"; break;
+	default:
+		break;
+	}
+
+}
+
 
 
 int main()
