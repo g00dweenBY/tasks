21aed37e2bd3c2492f8d6017783297c5ba163691
Dmitry Simonov <gundalini335@gmail.com>
2023-05-14 23:58:55 +0300

    task 6a

--- source.cpp
+++ source.cpp
@@ -5,6 +5,35 @@
 #include <vector>
 #include <algorithm>
 
+enum CardSuits
+{
+	SUIT_CLUBS,
+	SUIT_DIAMONDS,
+	SUIT_HEARTS,
+	SUIT_PEAKS,
+	MAX_SUITS,
+
+};
+
+enum CardTypes
+{
+	TYPE_TWO,
+	TYPE_THREE,
+	TYPE_FOUR,
+	TYPE_FIVE,
+	TYPE_SIX,
+	TYPE_SEVEN,
+	TYPE_EIGHT,
+	TYPE_NINE,
+	TYPE_TEN,
+	TYPE_JECK,
+	TYPE_LADY,
+	TYPE_KING,
+	TYPE_ACE,
+	MAX_TYPESM
+};
+
+
 int main()
 {
 	setlocale(LC_ALL, "Russian");
