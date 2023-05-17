c2f94de45f665b8e19e9d3fb1ac6ed6bb1665d75
Dmitry Simonov <gundalini335@gmail.com>
2023-05-17 09:15:17 +0300

    7.1 harmonicMean

--- source.cpp
+++ source.cpp
@@ -9,6 +9,53 @@
 #include <cmath>
 
 
+/*
+void arrayFuncttion(int arr[], int len, int value)
+{
+	for (int i = 0; i < len; ++i)
+	{
+		arr[i] = value;
+	}
+}
+
+double doubleFunc(double *arr, int length)
+{
+	double maxValue = 0;
+	for (int i = 0; i < length; ++i)
+	{
+		if (arr[i] > maxValue)
+			maxValue = arr[i];
+	}
+	return maxValue;
+}
+
+int judge(int (*func)(const char*));
+
+struct Aplicant
+{
+	char name[30];
+	int credit_ratings[3];
+};
+
+void funcStruct(Aplicant apl)
+{
+	std::cout << "name: " << apl.name << '\n';
+	std::cout << "rating: " << apl.credit_ratings << '\n';
+}
+
+void funcStructLink(Aplicant* apl)
+{
+	std::cout << apl->name;
+	std::cout << apl->credit_ratings;
+}
+
+void f1(Aplicant* a);
+const char* f2(const Aplicant* a1, const Aplicant* a2);
+
+typedef void(*f1_ptr)(Aplicant*);
+typedef const char* (*f2_ptr)(const Aplicant* p1, const Aplicant* p2);
+*/
+
 
 
 int main()
@@ -17,13 +64,7 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-
-
-
-
-
-
-
+	
 	return 0;
 }
 
---
+++ tasks/book-prata/07gl.cpp
@@ -0,0 +1,15 @@
+/*
+	int a, b;
+
+	std::cout << "enter two numbers: ";
+	while (std::cin >> a >> b)
+	{
+		if (a == 0 || b == 0)
+		{
+			std::cout << "The numbers must not be zero" << std::endl;
+			break;
+		}
+		double harmonicMean = 2.0 * a * b / (a + b);
+		std::cout << "harmonicMean = " << harmonicMean << std::endl;
+	}
+*/
