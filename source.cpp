363bed053f27013fa5f1f59aceddd5acae73ce27
Dmitry Simonov <gundalini335@gmail.com>
2023-06-02 05:06:18 +0300

    create stack

--- source.cpp
+++ source.cpp
@@ -15,29 +15,39 @@
 #include "namesp.h"
 #include <new>
 #include <cstdarg>
+#include <cassert>
 
 
-class Numbers
+class Stack
 {
-	int m_a;
-	int m_b;
-	int m_c;
+private:
+	int m_arr[10];
+	int m_next;
 public:
-	void setValues(int a, int b, int c)
+	void reset()
 	{
-		m_a = a;
-		m_b = b;
-		m_c = c;
+		m_next = 0;
+		for (int i = 0; i < 10; ++i)
+			m_arr[i] = 0;
 	}
-
+	bool push(int value)
+	{
+		if (m_next == 10)
+			return false;
+		m_arr[m_next++] = value;
+		return true;
+	}
+	int pop()
+	{
+		assert(m_next > 0);
+		return m_arr[--m_next];
+	}
 	void print()
 	{
-		std::cout << "<" << m_a << ", " << m_b << ", " << m_c << ">";
-	}
-
-	bool isEqual(const Numbers& d)
-	{
-		return (m_a == d.m_a && m_b == d.m_b && m_c == d.m_c);
+		std::cout << "( ";
+		for (int i = 0; i < m_next; ++i)
+			std::cout << m_arr[i] << " ";
+		std::cout << ")\n";
 	}
 };
 
@@ -48,21 +58,19 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	
-	Numbers p1;
-	p1.setValues(3, 4, 5);
-	Numbers p2;
-	p2.setValues(3, 4, 5);
-	if (p1.isEqual(p2))
-		std::cout << "p1 and p2 are equal\n";
-	else
-		std::cout << "p1 and p2 are not equal\n";
-	Numbers p3;
-	p3.setValues(7, 8, 9);
+	Stack stack;
+	stack.reset();
 
-	if (p1.isEqual(p3))
-		std::cout << "p1 and p3 are equal\n";
-	else
-		std::cout << "p1 and p3 are not equal\n";
+	stack.push(5);
+	stack.print();
+
+	stack.push(44);
+	stack.push(22);
+	stack.push(33);
+	stack.print();
+	stack.pop();
+	stack.print();
+	
 	
 	return 0;
 }
--- tasks/ravesli/08gl-oop/08glR-OOP.cpp
+++ tasks/ravesli/08gl-oop/08glR-OOP.cpp
@@ -1,2 +1,46 @@
 /*
+* ======================================================================================
+	
+	class Numbers
+{
+	int m_a;
+	int m_b;
+	int m_c;
+public:
+	void setValues(int a, int b, int c)
+	{
+		m_a = a;
+		m_b = b;
+		m_c = c;
+	}
+
+	void print()
+	{
+		std::cout << "<" << m_a << ", " << m_b << ", " << m_c << ">";
+	}
+
+	bool isEqual(const Numbers& d)
+	{
+		return (m_a == d.m_a && m_b == d.m_b && m_c == d.m_c);
+	}
+};
+
+	Numbers p1;
+	p1.setValues(3, 4, 5);
+	Numbers p2;
+	p2.setValues(3, 4, 5);
+	if (p1.isEqual(p2))
+		std::cout << "p1 and p2 are equal\n";
+	else
+		std::cout << "p1 and p2 are not equal\n";
+	Numbers p3;
+	p3.setValues(7, 8, 9);
+
+	if (p1.isEqual(p3))
+		std::cout << "p1 and p3 are equal\n";
+	else
+		std::cout << "p1 and p3 are not equal\n";
+* ======================================================================================
+ ======================================================================================
+ ======================================================================================
 */
